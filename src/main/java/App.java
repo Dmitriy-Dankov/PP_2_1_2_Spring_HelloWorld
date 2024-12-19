b50bf92d60f9e158b5cdaf1d9d08e765b9d96fbe
Dmitriy Dankov <d.a.n.k.o.v@yandex.ru>
2024-12-19 15:22:37 +0300

    add bin Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        
+        HelloWorld bean = context.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean1 = context.getBean("helloworld", HelloWorld.class);
+        
+        Cat catBean = context.getBean(Cat.class);
+        Cat catBean1 = context.getBean(Cat.class);
+
+        System.out.println(bean == bean1);
+        System.out.println(catBean == catBean1);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Scope("prototype")
+    @Bean(value="cat")
+    public Cat geCat() {
+        Cat cat = new Cat();
+        cat.setName("Барсик");
+        return cat;
+    }
 }
